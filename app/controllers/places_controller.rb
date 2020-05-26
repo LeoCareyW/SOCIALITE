caee2d84bb631097818891b956f27b7eb9453c31
Bradley Petty-Fitzmaurice <bradpfm@gmail.com>
2020-05-26 15:44:54 +0100

    IGNORE BROKEN

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,41 @@
 class PlacesController < ApplicationController
+
+def index
+  
+end
+
+def new
+  @places = Place.new
+end
+
+def create
+  @place = Place.new
+end
+
+def show 
+  @place = Place.find(params[:id])
+
+end
+
+def edit
+  @place = Place.find(params[:id])
+end
+
+def update 
+  @place = Place.find(params[:id])
+  @place.update(place_params)
+
+  if @place.save
+      redirect_to place_path(@place)
+  else
+      render :new
+  end
+end
+
+private
+
+def place_params
+  params.require(:place).permit(:name, :address, :category)
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Place.create(name: "McDonalds", address: "Ducklington Ln, Witney OX28 4JF", category: "Restaurant")
