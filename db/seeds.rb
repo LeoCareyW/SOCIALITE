261a34caff0cca105dcce2097cce28909552c38c
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-05-26 17:06:14 +0100

    pushing minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-places = Place.create({name: "Breakfast Club", address: "London", category: "Bar", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae debitis, quam, in corrupti, cum deserunt nulla maiores tempore soluta minima quae voluptatum dicta adipisci sapiente ipsum impedit veniam eos, voluptate."})
+breakfast_club = Place.create({name: "Breakfast Club", address: "London", category: "Bar", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae debitis, quam, in corrupti, cum deserunt nulla maiores tempore soluta minima quae voluptatum dicta adipisci sapiente ipsum impedit veniam eos, voluptate."})
 
 
 puts "checking..."
-places
+breakfast_club.save
 puts "completed making Breakfast Club"
+
