d7bf7a445f7853a3941dede3dae98f797240866a
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-05-28 15:57:11 +0100

    add a profile picture and basic profile layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :friendships
+  has_one_attached :photo
   has_many :friends, through: :friendships
   has_many :memberships
   has_many :groups, through: :memberships
