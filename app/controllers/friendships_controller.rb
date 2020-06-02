571d68759e210cf49285b419c7829d8ce5558925
Mikey Ettlinger <mikeyettlinger@gmail.com>
2020-06-02 10:04:56 +0100

    dont pull

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,10 +3,8 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
     if @friendship.save
       flash[:notice] = "Added frienship"
-      redirect_to root_url
     else
       flash[:error]
-      redirect_to root_url
     end
   end
 
