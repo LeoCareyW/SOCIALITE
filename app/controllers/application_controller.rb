7d221892ff77c2d3da6a199da17a2dd99f58daf1
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-06-03 14:30:44 +0100

    profile styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :unread_messages_count
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :ip_address, :latitude, :longitude])
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :ip_address, :latitude, :longitude, :comment])
-  end
-
-  def unread_messages_count
-    @count = current_user.messages.where(read: false).count
   end
 end
