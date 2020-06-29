b66735cec00d725572829ec2726540335e7cc118
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-06-29 17:17:18 +0100

    logged out users now have reduced functionality

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -37,10 +37,13 @@ def show
   @places = Place.where(:category => @place.category)
   @markers = [{ lat: @place.latitude, lng: @place.longitude }]
   # @groups = Group.joins(:users).where(users: current_user)
-  @groups = current_user.groups
-  @friends = current_user.friends
-  @comment = Comment.new
-  @recommendation = Recommendation.where("user_id = ? and place_id = ?", current_user.id, @place.id).first
+  if user_signed_in?
+    @groups = current_user.groups
+    @friends = current_user.friends
+    @comment = Comment.new
+    @recommendation = Recommendation.where("user_id = ? and place_id = ?", current_user.id, @place.id).first
+  else
+  end
 end
 
 
